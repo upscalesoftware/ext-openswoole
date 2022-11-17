9eec798d724ea03f8755ee6b0b497bd59c4c9ed5
Sergii Shymko <sergey@shymko.net>
2022-11-16 19:59:06 -0600

    Fetch release info besides tag name
    - Use JSON-proof EOF delimiter with unescaped backslash ~\~
    - Mirror release name, tag, draft, and prerelese fields
    - Link official release notes in release description
    - Import releases from oldest to latest

--- .github/workflows/fetch_releases.sh
+++ .github/workflows/fetch_releases.sh
@@ -5,5 +5,5 @@ repo="$1"
 curl -s "https://api.github.com/repos/$repo/releases?per_page=100&page=[1-5]" \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
-  | jq '[ .[].tag_name | ltrimstr("swoole-") ]' \
+  | jq '.[].tag_name |= ltrimstr("swoole-")' \
   | jq -s add

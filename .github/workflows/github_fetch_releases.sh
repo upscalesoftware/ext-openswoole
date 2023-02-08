d60d2e73a4b8ece9632ba8c06bd10d321c65ba22
Sergii Shymko <sergey@shymko.net>
2023-02-07 19:16:49 -0600

    Follows redirects when fetching releases

--- .github/workflows/github_fetch_releases.sh
+++ .github/workflows/github_fetch_releases.sh
@@ -2,7 +2,7 @@
 
 repo="$1"
 
-curl -s "https://api.github.com/repos/$repo/releases?per_page=100&page=[1-5]" \
+curl -s -L "https://api.github.com/repos/$repo/releases?per_page=100&page=[1-5]" \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
   | jq '.[] |= if (.name == "") then .name = .tag_name else . end' \
